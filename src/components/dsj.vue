75d87dc87a3f0888ed34efa3f699331e29e544a2
刘赛 <ls42675@ly.com>
2017-10-27 15:54:45 +0800

    修改返回 Signed-off-by: 刘赛 <ls42675@ly.com>

--- src/components/dsj.vue
+++ src/components/dsj.vue
@@ -2,9 +2,15 @@
   <div class="dsj">
      <img src="../../static/dsj/dsj_01.png"/>
      <img src="../../static/dsj/dsj_02.png"/>
-     <div class="back" @click="back()">
+     <div class="back" @click="backpage()">
      	  <img src="../../static/dsj/return.png"/>
      </div>
+    <audio id="clickMusic" src="./static/clickaudio.mp3" >
+    </audio>
+    <!--导航-->
+    <div class="nav" @click="back">
+      <img src="../../static/jwl/top.png"/>
+    </div>
   </div>
 </template>
 
@@ -13,18 +19,35 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      audiodom : ''
     }
   },
+  mounted(){
+    var self = this;
+    self.audiodom = document.getElementById('clickMusic')
+    window.addEventListener('scroll', this.handleScroll);
+  },
   methods: {
-  	back(){
+  	backpage(){
   		  var self = this
-				// self.audiodom.play();
+				 self.audiodom.play();
 				setTimeout(function(){
 					self.$emit('listenToChildEvent',0)
 				},300)
-        
-  	}
+
+  	},
+    handleScroll :function(){
+      this.scrolled = window.scrollY > 0;
+      if(window.scrollY>680){
+        $('.nav').show();
+      }else{
+        $('.nav').hide();
+      }
+    },
+    back:function(){
+      $('body,html').animate({ scrollTop: 0 }, 200);
+    }
   }
 }
 </script>
@@ -39,5 +62,13 @@ export default {
 	left: 10px;
 	top: 10px;
 	width: 44px;
+}
+.nav{
+  position: fixed;
+  top: 50%;
+  right: 10px;
+  width:44px;
+  display: none;
+  z-index: 9999;
 }
 </style>
